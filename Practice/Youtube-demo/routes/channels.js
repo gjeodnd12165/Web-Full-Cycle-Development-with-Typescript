ce5dd70ceb11f99cacafad00e398bae9e479ba6e
gjeodnd12165 <princewoong@gmail.com>
2024-03-26 11:05:54 +0900

    add, improve routers

--- Practice/Youtube-demo/routes/channels.js
+++ Practice/Youtube-demo/routes/channels.js
@@ -1,51 +1,65 @@
 const express = require('express');
-const app = express();
-app.listen(7777, () => console.log(`Server started, on port ${7777}`));
+const router = express.Router();
 
-app.use(express.json());
+router.use(express.json());
 
 
 // id -> {userid, title, description}
 let db = new Map();
 let id = 1;
 
-app
-  .route('/channels')
+router
+  .route('/')
   .post((req, res) => {
-    const { channelTitle } = req.body;
-    if (!(channelTitle)) {
+    const { channelTitle, userId } = req.body;
+    if (!(channelTitle && userId)) {
       res.status(400).json({
         message: `양식이 잘못 되었을 수 있습니다`
       });
     }
+    else if ([...db.keys()].includes(userId)) {
+      res.status(404).json({
+        message: `해당하는 유저가 없습니다`
+      });
+    }
     else {
       db.set((id++).toString(), {
-        channelTitle: channelTitle
+        channelTitle: channelTitle,
+        userId: userId
       });
       res.status(201).json({
         message: `${channelTitle} 채널이 생성되었습니다`
-      })
+      });
     }
 })// 채널 생성
   .get((req, res) => {
-    if (!db.size) {
+    const { userId } = req.body;
+    if (!userId) {
       res.status(404).json({
-        message: "채널이 존재하지 않습니다"
+        message: "로그인이 필요한 페이지입니다"
       });
     }
     else {
-      res.status(200).json([...db.values()]);
+      const filtered = [...db.values()].filter((channel) => {
+        return channel.userId === userId;
+      });
+      if (!filtered.length) {
+        channelNotFound();
+      }
+      else {
+        res.status(200).json([...db.values()].filter((channel) => {
+          return channel.userId === userId;
+        }));
+      }
     }
 })// 채널 전체 조회
-app
-  .route('/channels/:id')
+router
+  .route('/:id')
   .put((req, res) => {
     const { id } = req.params;
     const { channelTitle } = req.body;
     if (!db.has(id)) {
-      res.status(404).json({
-        message: `해당하는 채널이 존재하지 않습니다`
-      });
+      channelNotFound();
     }
     else {
       const channel = db.get(id);
@@ -61,9 +75,7 @@ app
   .delete((req, res) => {
     const { id } = req.params;
     if (!db.has(id)) {
-      res.status(404).json({
-        message: `해당하는 채널이 존재하지 않습니다`
-      });
+      channelNotFound();
     }
     else {
       const { channelTitle } = db.get(id); 
@@ -76,11 +88,17 @@ app
   .get((req, res) => {
     const { id } = req.params;
     if (!db.has(id)) {
-      res.status(404).json({
-        message: `해당하는 채널이 존재하지 않습니다`
-      });
+      channelNotFound();
     }
     else {
       res.status(200).json(db.get(id));
     }
 });//채널 개별 조회
+
+const channelNotFound = () => {
+  res.status(404).json({
+    message: `해당하는 채널이 존재하지 않습니다`
+  });
+}
+
+module.exports = router;
--- Practice/Youtube-demo/routes/users.js
+++ Practice/Youtube-demo/routes/users.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const app = express();
-app.listen(7777, () => console.log(`Server started, on port ${7777}`));
+const router = express.Router();
 
-app.use(express.json());
+router.use(express.json());
 
 
 // id -> {pwd, name}
 let db = new Map();
 
 // 로그인
-app.post('/signin', (req, res) => {
-  const {id, pwd} = req.body;
-  if (!db.has(id)) {
+router.post('/signin', (req, res) => {
+  const {userId, pwd} = req.body;
+  if (!db.has(userId)) {
     res.status(404).json({
       message: "해당 유저를 찾을 수 없습니다"
     });
   }
-  else if (db.get(id).pwd !== pwd) {
+  else if (db.get(userId).pwd !== pwd) {
     res.status(404).json({
       message: "아이디 혹은 비밀번호가 다릅니다"
     });
@@ -28,20 +27,20 @@ app.post('/signin', (req, res) => {
   }
 });
 // 회원가입
-app.post('/signup', (req, res) => {
-  const {id, pwd, name} = req.body;
-  if (!(id && pwd && name)) {
+router.post('/signup', (req, res) => {
+  const {userId, pwd, name} = req.body;
+  if (!(userId && pwd && name)) {
     res.status(400).json({
       message: "양식이 잘 못 되었을 수 있습니다"
     });
   }
-  else if (db.has(id)) {
+  else if (db.has(userId)) {
     res.status(400).json({
       message: "id가 중복됩니다"
     });
   }
   else {
-    db.set(id, {
+    db.set(userId, {
       pwd: pwd,
       name: name
     });
@@ -51,32 +50,34 @@ app.post('/signup', (req, res) => {
   }
 });
 // 회원 개별 조회
-app
-  .route('/users/:id')
+router
+  .route('/users')
   .get((req, res) => {
-  const {id} = req.params;
-  if (!db.has(id)) {
+  const {userId} = req.body;
+  if (!db.has(userId)) {
     res.status(404).json({
       message: "해당 유저를 찾을 수 없습니다"
     });
   }
   else {
-    res.json(db.get(id));
+    res.json(db.get(userId));
   }
 })
 // 회원 개별 탈퇴
   .delete((req, res) => {
-  const {id} = req.params;
-  if (!db.has(id)) {
+  const {userId} = req.body;
+  if (!db.has(userId)) {
     res.status(404).json({
       message: "해당 유저를 찾을 수 없습니다"
     });
   }
   else {
-    const name = db.get(id).name;
-    db.delete(id);
+    const name = db.get(userId).name;
+    db.delete(userId);
     res.json({
       message: `${name}님, 안녕히가세요`
     });
   }
 });
+
+module.exports = router;
